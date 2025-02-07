b386529494d8c28a01f2e31eaaa9b9c91a338f4e
Mohit Saklani <mohit200648saklani@gmail.com>
2025-02-08 00:54:13 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -18,9 +18,11 @@ int main() {
         printf("%d",result);
         break;
         case '/':
-        result=a/b;
-        printf("%d",result);
-        break;
+        if (b != 0)  // Checking division by zero
+                printf("Result: %d\n", a / b);
+            else
+                printf("Error: Division by zero is not allowed.\n");
+            break;
         default:
         printf("Invalid Operator");
         break;
